450caf775a7062407d152b7e19d5a4e5388dad3b
Abdulbari322 <abdulatbari@gmail.com>
2023-06-04 17:31:13 +1000

    test2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './components/header'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <h1>Welcome to github</h1>
+    <>
+      <Header />
+      <main className = "py-3">
+        <Container>
+          <h1>Welcome to nothing</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+      
   )
 }
 
