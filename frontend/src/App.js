d134191573a69448905de6fec2abe75c8ebe969d
Abdulbari322 <abdulatbari@gmail.com>
2023-06-11 16:02:22 +1000

    front and backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
+import React from 'react';
 import { Container } from 'react-bootstrap'
+import {Outlet} from 'react-router-dom'
 import Header from './components/header'
 import Footer from './components/Footer'
+
 
 const App = () => {
   return (
@@ -8,7 +11,7 @@ const App = () => {
       <Header />
       <main className = "py-3">
         <Container>
-          <h1>Welcome to nothing</h1>
+          <Outlet />
         </Container>
       </main>
       <Footer />
