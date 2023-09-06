689334d33f77499716c3ebfa434dc1366807e8d7
Abdulbari322 <abdulatbari@gmail.com>
2023-09-07 04:48:21 +1000

    sep 7 update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,56 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 //import 'bootstrap/dist/css/bootstrap.min.css'
 import './assets/styles/bootstrap.custom.css'
-import './assets/styles/index.css';
-import App from './App';
-import { Provider } from 'react-redux';
-import store from './store';
-import reportWebVitals from './reportWebVitals';
+import './assets/styles/index.css'
+import App from './App'
+import { Provider } from 'react-redux'
+import store from './store'
+import reportWebVitals from './reportWebVitals'
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
-  RouterProvider
-} from 'react-router-dom';
-import HomeScreen from './screens/HomeScreen';
-import ProductScreen from './screens/ProductScreen';
-import CartScreen from './screens/cartScreen';
-import LoginScreen from './screens/LoginScreen';
-import RegisterScreen from './screens/RegisterScreen';
-import ShippingScreen from './screens/ShippingScreen';
-import PrivateRoute from './components/PrivateRoute';
-import PaymentScreen from './screens/PaymentScreen';
+  RouterProvider,
+} from 'react-router-dom'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
+import CartScreen from './screens/cartScreen'
+import LoginScreen from './screens/LoginScreen'
+import RegisterScreen from './screens/RegisterScreen'
+import ShippingScreen from './screens/ShippingScreen'
+import PrivateRoute from './components/PrivateRoute'
+import PaymentScreen from './screens/PaymentScreen'
+import PlaceOrderScreen from './screens/PlaceOrderScreen'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<App />}> 
-      <Route index={true} path='/' element={<HomeScreen />}/>
-      <Route path='/product/:id' element={<ProductScreen />}/>
-      <Route path='/cart' element={<CartScreen/>}/>
-      <Route path='/login' element={<LoginScreen/>}/>
-      <Route path='/register' element={<RegisterScreen/>}/>
-      
+    <Route path='/' element={<App />}>
+      <Route index={true} path='/' element={<HomeScreen />} />
+      <Route path='/product/:id' element={<ProductScreen />} />
+      <Route path='/cart' element={<CartScreen />} />
+      <Route path='/login' element={<LoginScreen />} />
+      <Route path='/register' element={<RegisterScreen />} />
 
-      <Route path='' element={<PrivateRoute/>}>
-        <Route path='/shipping' element={<ShippingScreen/>}/>
+      <Route path='' element={<PrivateRoute />}>
+        <Route path='/shipping' element={<ShippingScreen />} />
         <Route path='/payment' element={<PaymentScreen />} />
+        <Route path='/placeorder' element={<PlaceOrderScreen />} />
       </Route>
-        
     </Route>
   )
-);
+)
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <Provider store = {store}>
-     <RouterProvider router={router}/>
+    <Provider store={store}>
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
---
+++ frontend/src/screens/PlaceOrderScreen.jsx
@@ -0,0 +1,96 @@
+import React from 'react'
+import { useEffect } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import {
+  Button,
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Card,
+  ListGroupItem,
+} from 'react-bootstrap'
+import { toast } from 'react-toastify'
+import Message from '../components/Message'
+import CheckoutSteps from '../components/CheckoutSteps'
+import Loader from '../components/Loader'
+import { useCreateOrderMutation } from '../slices/ordersApiSlice'
+import { clearCartItems } from '../slices/cartSlice'
+
+const PlaceOrderScreen = () => {
+  const navigate = useNavigate()
+  const cart = useSelector((state) => state.cart)
+
+  useEffect(() => {
+    if (!cart.shippingAddress.address) {
+      navigate('/shipping')
+    } else if (!cart.paymentMethod) {
+      navigate('/payment')
+    }
+  }, [cart.paymentMethod, cart.shippingAddress.address, navigate])
+
+  return (
+    <>
+      <CheckoutSteps step1 step2 step3 step4 />
+      <Row>
+        <Col md={8}>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h2>Shipping</h2>
+              <p>
+                <strong>Address:</strong>
+                {cart.shippingAddress.address},{' '}
+                {cart.shippingAddress.address.city}{' '}
+                {cart.shippingAddress.postalCode},{' '}
+                {cart.shippingAddress.country}
+              </p>
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              <h2>Payment Method</h2>
+              <strong>Method: </strong>
+              {cart.paymentMethod}
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              <h2>Order Items</h2>
+              {cart.cartItems.length === 0 ? (
+                <Message>Your cart is empty</Message>
+              ) : (
+                <ListGroup variant='flush'>
+                  {cart.cartItems.map((item, index) => (
+                    <ListGroup.Item key={index}>
+                      <Row>
+                        <Col md={1}>
+                          <Image
+                            src={item.image}
+                            alt={item.name}
+                            fluid
+                            rounded
+                          />
+                        </Col>
+                        <Col>
+                          <Link to={`/products/${item.product}`}>
+                            {item.name}
+                          </Link>
+                        </Col>
+                        <Col md={4}>
+                          {item.qty} x ${item.price} = $
+                          {(item.qty * item.price).toFixed(2)}
+                        </Col>
+                      </Row>
+                    </ListGroup.Item>
+                  ))}
+                </ListGroup>
+              )}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={4}>Column</Col>
+      </Row>
+    </>
+  )
+}
+
+export default PlaceOrderScreen
