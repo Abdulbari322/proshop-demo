ae2a262b4db8865c5c2bdf255d5406ad0f22cfdb
Abdulbari322 <abdulatbari@gmail.com>
2023-09-19 14:06:16 +1000

    sep 19

--- Backend/server.js
+++ Backend/server.js
@@ -1,37 +1,40 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
-dotenv.config();
-import connectDB from './config/db.js';
-import productRoutes from './routes/productRoutes.js';
-import userRoutes from './routes/userRoutes.js';
-import orderRoutes from './routes/orderRoutes.js';
-import {notFound, errorHandler} from './middleware/errorMiddleware.js'
-import products from './data/products.js';
+import express from 'express'
+import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
+dotenv.config()
+import connectDB from './config/db.js'
+import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+import orderRoutes from './routes/orderRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import products from './data/products.js'
 
+const port = process.env.PORT || 6000
 
-const port = process.env.PORT || 6000;
+connectDB() //connect to mongoDB
 
-connectDB(); //connect to mongoDB
-
-const app = express();
+const app = express()
 
 //body parser middleware
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 //cookie parser middleware
-app.use(cookieParser());
+app.use(cookieParser())
 
 app.get('/', (req, res) => {
-    res.send('API is running');
-});
+  res.send('API is running')
+})
 
-app.use('/api/products', productRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/orders', orderRoutes);
+app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes)
 
-app.use(notFound);
-app.use(errorHandler);
+app.get('/api/config/paypal', (req, res) =>
+  res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
+)
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.use(notFound)
+app.use(errorHandler)
+
+app.listen(port, () => console.log(`Server is running on port ${port}`))
--- frontend/src/utils/cartUtils.js
+++ frontend/src/utils/cartUtils.js
@@ -1,21 +1,28 @@
 export const addDecimals = (num) => {
-    return (Math.round(num * 100) / 100).toFixed(2);
-};
+  return (Math.round(num * 100) / 100).toFixed(2)
+}
 
+export const updateCart = (state) => {
+  // Calculate the items price
+  state.itemsPrice = addDecimals(
+    state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+  )
 
-export const updateCart = (state) =>{
-    //calculate items price
-state.itemsprice = addDecimals(state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0));
-//calculate shipping price (if order is over $100 free shipping else $10)
-state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
-//calculate tax price
-state.taxPrice = addDecimals(Number((0.15 * state.itemsPrice).toFixed(2)));
-//calculate total price
-state.totalPrice = (
-    Number(state.itemsPrice) + Number(state.shippingPrice) + Number(state.taxPrice)
-).toFixed(2);
+  // Calculate the shipping price
+  state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10)
 
-    localStorage.setItem('cart', JSON.stringify(state));
+  // Calculate the tax price
+  state.taxPrice = addDecimals(Number((0.15 * state.itemsPrice).toFixed(2)))
 
-    return state;
+  // Calculate the total price
+  state.totalPrice = (
+    Number(state.itemsPrice) +
+    Number(state.shippingPrice) +
+    Number(state.taxPrice)
+  ).toFixed(2)
+
+  // Save the cart to localStorage
+  localStorage.setItem('cart', JSON.stringify(state))
+
+  return state
 }
